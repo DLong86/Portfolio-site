02ab2931c0b6ec565f7bbce472653893c5f93baa
David Long <74575072+DLong86@users.noreply.github.com>
2022-05-30 11:27:15 +0900

    Update images.js

--- src/pages/projects/images.js
+++ src/pages/projects/images.js
@@ -3,16 +3,19 @@ const images = [
 		id: 1,
 		src: 'https://images.unsplash.com/photo-1519692933481-e162a57d6721?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170',
 		alt: '...',
+		description: 'Project 1',
 	},
 	{
 		id: 2,
 		src: 'https://images.unsplash.com/photo-1514897575457-c4db467cf78e?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170',
 		alt: '...',
+		description: 'Project 2',
 	},
 	{
 		id: 3,
 		src: 'https://images.unsplash.com/photo-1652523843280-1266e2520a91?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1167',
 		alt: '...',
+		description: 'Project 3',
 	},
 ];
 export default images;
