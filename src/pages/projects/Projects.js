c6f67892d9693d8bc527d797013fa6873402cd80
David Long <74575072+DLong86@users.noreply.github.com>
2022-05-30 10:34:35 +0900

    Update Projects.js

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -1,8 +1,36 @@
-const Projects = () => {
+import { useState, useEffect } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import styles from './Projects.module.css';
+
+const Projects = ({ images }) => {
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	};
 	return (
-		<div>
-			<h1>Projects page</h1>
-			<p>Projects will go here</p>
+		<div className={styles.projectPage}>
+			<div className={styles.tag}>
+				<p>Just a few bits and pieces...</p>
+			</div>
+			<div className={styles.imgslider}>
+				<p>description goes here dragged from value from images.js objects</p>
+				<Slider {...settings}>
+					{images.map(item => (
+						<div key={item.id}>
+							<img
+								className={styles.projectImg}
+								src={item.src}
+								alt={item.alt}
+							/>
+						</div>
+					))}
+				</Slider>
+			</div>
 		</div>
 	);
 };
