7b6f06b3b0e82ac7060634be6dabb5e9bff7590c
David Long <74575072+DLong86@users.noreply.github.com>
2022-06-07 12:03:18 +0900

    Update Projects.js

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -9,29 +9,30 @@ const Projects = ({ images }) => {
 		<div className={styles.projects}>
 			<div className={styles['image-section']}>
 				<div className={styles['image-box']}>
-					<h3>Sane & Able</h3>
-					<p>Landing Page</p>
+					<h3>Skate Gallery</h3>
+					<small>App for skateboarders to upload photos</small>
 					<div className={styles['image1']}></div>
 				</div>
 				<div className={styles['image-box']}>
 					<h3>Sane & Able</h3>
-					<p>Landing Page</p>
+					<p>deanpiper.com</p>
 					<div className={styles['image2']}></div>
 				</div>
 				<div className={styles['image-box']}>
-					<h3>Sane & Able</h3>
-					<p>Landing Page</p>
+					<h3>F1 Driver List</h3>
+					<small>Driver list and stats for 2022 season</small>
 					<div className={styles['image3']}></div>
 				</div>
 				<div className={styles['image-box']}>
-					<h3>Sane & Able</h3>
-					<p>Landing Page</p>
-					<div className={styles['image3']}></div>
+					<h3>Ends</h3>
+					<p>Home page</p>
+					<small>website for a clothing company</small>
+					<div className={styles['image4']}></div>
 				</div>
 				<div className={styles['image-box']}>
-					<h3>Sane & Able</h3>
-					<p>Landing Page</p>
-					<div className={styles['image3']}></div>
+					<h3>Ends</h3>
+					<small>Photo gallery page</small>
+					<div className={styles['image5']}></div>
 				</div>
 			</div>
 		</div>
@@ -39,4 +40,3 @@ const Projects = ({ images }) => {
 };
 
 export default Projects;
-
