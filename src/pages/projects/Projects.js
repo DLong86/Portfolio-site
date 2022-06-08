15a91eca3f3220b94073e32dda787fa8bbbf69f5
David Long <74575072+DLong86@users.noreply.github.com>
2022-06-08 10:56:17 +0900

    Update Projects.js

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -1,7 +1,4 @@
 import { useState, useEffect } from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import styles from './Projects.module.css';
 
 const Projects = ({ images }) => {
